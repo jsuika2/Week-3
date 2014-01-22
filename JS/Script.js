8cce26ebb96d12847560c62d3b555d656d3d1ba9
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-21 22:13:16 -0500

    Conditional logic, this time adding <  or > to different varriables and results go to console

--- JS/Script.js
+++ JS/Script.js
@@ -2,19 +2,18 @@
 //conditional logic   done on jan 21
 
 
+ var kidHeight =52;
+ var midHeight =48;
+                                                                                                //if child is old enough, print to the console
 
- var oldEnough= true;
-
-//if child is old enough, print to the console
-
-if(oldEnough){
-                             //code preformed if condition is true
-    console.log("You can ride the coaster!");
+if(kidHeight > midHeight){                                                                                //If the kid is over 48 inches in height
+                                                                                                 //code preformed if condition is true
+    console.log("You can ride the coaster!");                       //Suika "the is the action"
 
 
 }
 
 
-console.log("What comes after?");
+
 
 
