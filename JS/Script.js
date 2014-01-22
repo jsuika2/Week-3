be5c5b57bea8a05ac0511af5ba12a9fc4ac2ab38
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-21 23:57:41 -0500

    conditional logic, this is with two blocks, about to enter a third block

--- JS/Script.js
+++ JS/Script.js
@@ -1,25 +1,17 @@
 
-//conditional logic if else                    done on jan 21
+//conditional logic logical operations-js                    done on jan 21
 
 
- var kidHeight =52;
- var midHeight =48;
- var wParentHeight =45;                                                                     //height of the kid and parent
-                                                                                                //if child is old enough, print to the console
+var budget =300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
-if(kidHeight > midHeight){                                                                                //If the kid is over 48 inches in height
-                                                                                                 //code preformed if condition is true
-    console.log("You can ride the coaster!");                       //Suika "the is the action"
 
-}
 
-else if(kidHeight > wParentHeight){
-    console.log("You can ride but only with a parent");
+                                                        //if the price of the phone is less then our budget and paycheck is over 300
+
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
 }else{
-    console.log("Sorry kid you got some growing to do");
-
+    console.log("Sorry, not Phone for you!");
 }
-
-
-
-
