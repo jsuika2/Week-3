2fc6714431216639180a9cdf550512fdc13650c1
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-21 20:54:36 -0500

    Intial commit

---
+++ JS/Script.js
@@ -0,0 +1,21 @@
+
+
+/**
+ * Created by Suika on 1/21/14.
+ */
+
+
+---------------
+
+
+
+
+if(sunny){
+    goToTheBeach();
+
+    if(warmWater){
+        wearNewSuit();
+    }
+}else{
+    goToTheMovies();
+}
