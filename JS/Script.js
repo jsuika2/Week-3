bc27ed78b12fa166aaa675ceaa1e00f548ad7c7e
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-22 00:14:56 -0500

    added the || part

--- JS/Script.js
+++ JS/Script.js
@@ -2,15 +2,15 @@
 //conditional logic logical operations-js                    done on jan 21
 
 
-var budget =300;
+var budget =100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 
 
                                                         //if the price of the phone is less then our budget and paycheck is over 300
 
-if(iPhonePrice < budget && paycheck < 300){
+if(iPhonePrice < budget || wonLottery===true){
     console.log("We can buy the phone!");
 }else{
     console.log("Sorry, not Phone for you!");
