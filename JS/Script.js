2f45fdac62d82d91ec0905dbc767854a3ec05fa9
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-21 22:53:46 -0500

    using now the else and if conditions now because I am using three blocks of varriables.

--- JS/Script.js
+++ JS/Script.js
@@ -1,9 +1,10 @@
 
-//conditional logic   done on jan 21
+//conditional logic if else                    done on jan 21
 
 
- var kidHeight =47;
+ var kidHeight =52;
  var midHeight =48;
+ var wParentHeight =45;                                                                     //height of the kid and parent
                                                                                                 //if child is old enough, print to the console
 
 if(kidHeight > midHeight){                                                                                //If the kid is over 48 inches in height
@@ -12,8 +13,11 @@ if(kidHeight > midHeight){                                                      
 
 }
 
-else{
+else if(kidHeight > wParentHeight){
+    console.log("You can ride but only with a parent");
+}else{
     console.log("Sorry kid you got some growing to do");
+
 }
 
 
