01258f873c0ccd970c2fdf4879b8aa5ed1a34885
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-28 12:52:28 -0500

    Function invocation or CALL

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,5 @@
-
-
-
 //notes from ScreenCast   jan 28 2014
+// Function Execution
 
 //prompt("hello");
 
@@ -11,14 +9,15 @@ function outptMsg(){
 }
 
 function calcArea(){
-    var width =20;
+    var width =20;                            //fuction difination
     var height=30;
     var area =width*height;
     console.log(area);
 }
 
-
-
+calcArea();   //invocation of the fuction    or CALL, FUNCTION CALL
+calcArea()
+calcArea()
 
 
 
