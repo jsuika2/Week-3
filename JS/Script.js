89f63e903e39f2b7d6cddb3b0ed810e51014c508
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-28 12:48:18 -0500

    function Execution

--- JS/Script.js
+++ JS/Script.js
@@ -3,11 +3,19 @@
 
 //notes from ScreenCast   jan 28 2014
 
+//prompt("hello");
 
 
+function outptMsg(){
+    console.log("Hello World");
+}
 
-
-
+function calcArea(){
+    var width =20;
+    var height=30;
+    var area =width*height;
+    console.log(area);
+}
 
 
 
