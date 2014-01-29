05d59c0396e2581da8a6e60aa59c4576aec30928
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-29 10:23:30 -0500

    define parameters, and inputed 2 different aurgumenrs

--- JS/Script.js
+++ JS/Script.js
@@ -8,20 +8,24 @@
   //  console.log(area);
 
 
-function dogYears(){
-    var age =4;
-    var dogYears = age *7
+//function dogYears(){
+    //var age =4;
+    //var dogYears = age *7
+   // console.log("Sparky is " + dogYears + " years old.")                ///First part before parameters are placed
+//}
+
+//dogYears();       // Calling it
+
+
+
+function dogYears(age){                                          //----------the AGE holds the parameters------
+    var dogYears = age *5.5
     console.log("Sparky is " + dogYears + " years old.")
 }
 
-dogYears();       // Calling it
+dogYears(6);       // Calling it        ------The 6 holds augments--------
+dogYears(13);              // The input of 6, or 13, is the "PASS" or "PASSING"
 
-
-
-
-
-
-
-
+                    //"We are Passing the vale 6 to age."
 
 
